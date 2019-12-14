9c9f267ba69ba4ddca50761b4d59aaa47eb98e98
loan-petit <petit.loan1@gmail.com>
2019-12-14 16:03:42 +0100

    feature: Responsive home page header.

--- flutter/lib/src/ui/screens/home.dart
+++ flutter/lib/src/ui/screens/home.dart
@@ -7,14 +7,12 @@ import 'package:portfolio/src/ui/widgets/shared/scaffold.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return AppScaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          SizedBox(height: screenHeight / 8),
+          SizedBox(height: 32.0),
           ProfileHeader(),
         ],
       ),
--- flutter/lib/src/ui/widgets/profile_header.dart
+++ flutter/lib/src/ui/widgets/profile_header.dart
@@ -4,29 +4,79 @@ import 'package:flutter/material.dart';
 class ProfileHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 600) {
+        return _buildForSmallScreen(context);
+      } else {
+        return _buildForLargeScreen(context);
+      }
+    });
+  }
+
+  Widget _buildForSmallScreen(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.display3;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          radius: 100.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/png/profile.png'),
+        ),
+        SizedBox(height: 32.0),
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: textStyle.apply(fontWeightDelta: 3),
+            children: <TextSpan>[
+              TextSpan(text: "Hello, I'm "),
+              TextSpan(
+                text: 'Loan PETIT',
+                style: textStyle.apply(
+                    fontWeightDelta: 3,
+                    color: Theme.of(context).colorScheme.primary),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForLargeScreen(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.display3;
+
     return Row(
       children: <Widget>[
+        Expanded(
+          child: Align(
+            alignment: Alignment.center,
+            child: CircleAvatar(
+              radius: 100.0,
+              backgroundColor: Colors.transparent,
+              backgroundImage: AssetImage('assets/png/profile.png'),
+            ),
+          ),
+        ),
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Text(
                 'Hello,',
-                style: Theme.of(context).textTheme.display4.apply(
-                      fontWeightDelta: 2,
-                    ),
+                style: textStyle.apply(
+                  fontWeightDelta: 2,
+                ),
               ),
               RichText(
                 text: TextSpan(
-                  style: Theme.of(context)
-                      .textTheme
-                      .display4
-                      .apply(fontWeightDelta: 3),
+                  style: textStyle.apply(fontWeightDelta: 3),
                   children: <TextSpan>[
                     TextSpan(text: "I'm "),
                     TextSpan(
                       text: 'Loan PETIT',
-                      style: Theme.of(context).textTheme.display4.apply(
+                      style: textStyle.apply(
                           fontWeightDelta: 3,
                           color: Theme.of(context).colorScheme.primary),
                     ),
@@ -34,16 +84,6 @@ class ProfileHeader extends StatelessWidget {
                 ),
               ),
             ],
-          ),
-        ),
-        CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 120.0,
-          child: ClipOval(
-            child: Image.asset(
-              'assets/png/profile_alpha.png',
-              fit: BoxFit.cover,
-            ),
           ),
         ),
       ],
